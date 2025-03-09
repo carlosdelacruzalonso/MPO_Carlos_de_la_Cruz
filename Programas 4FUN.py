e147aca05b3814193d48ec16fc6207d558242d14
Carlos <carlosdelacruzalonso@gmail.com>
2025-03-09 21:29:09 +0100

    Update Programas 4FUN.py
    Mini programas para practicar

--- Programas 4FUN.py
+++ Programas 4FUN.py
@@ -1,6 +1,17 @@
-#Fahrenheit to Celsius program
+#Mini app - Fahrenheit to Celsius program
 
 fahrenheit_to_celsius = input("Write here the temperature in fahrenheit to convert it to Celsius: ")
 to_int = int(fahrenheit_to_celsius)
 celsius = (to_int - 32) / 1.8
 print(f"Equivale a {round(celsius, 2)} °C")
+
+#Mini app - Body mass index
+
+peso = int(input("Escribe aquí tu peso en kg: "))
+altura = float(input("Escribe aquí tu altura en metros: "))
+
+i_m_c = peso / (altura**2)
+
+print(f"Tu indice de masa corporal es: {round(i_m_c, 2)}")
+
+
