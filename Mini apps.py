c3ffe4b187e941d43bbb6033ad18ba0b440f4464
Carlos <carlosdelacruzalonso@gmail.com>
2025-03-15 18:28:32 +0100

    Update Mini apps.py

--- Mini apps.py
+++ Mini apps.py
@@ -22,3 +22,20 @@ resultado = (cateto1**2 + cateto2**2)
 hipotenusa = (resultado ** 0.5)
 
 print(f"La longitud de la hipotenusa es: {round(hipotenusa, 2)}")
+
+#Mini app 4 - Spare change in Colombian pesos, peruvian soles and brazilian reais to EUR.
+pesos_left = float(input("What do you have left in pesos? "))
+soles_left = float(input("What do you have left in soles? "))
+reales_left = float(input("What do you have left in reales? "))
+
+peso_a_euro = 0.00022
+sol_a_euro = 0.25
+real_a_euro = 0.1590
+
+peso_euro = pesos_left * peso_a_euro
+sol_euro = soles_left * sol_a_euro
+real_euro = reales_left * real_a_euro
+
+eur_total = peso_euro + sol_euro + real_euro
+
+print(f"You have a total of {round(eur_total, 2)} €")
