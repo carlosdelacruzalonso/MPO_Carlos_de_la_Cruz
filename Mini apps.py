0e737eae1f176e247839afae3e52c2ea1095b33e
Carlos <carlosdelacruzalonso@gmail.com>
2025-03-30 10:46:02 +0200

    Update Mini apps.py

--- Mini apps.py
+++ Mini apps.py
@@ -184,3 +184,23 @@ else:
 
 print("Thank you for sharing your level of satisfaction! We hope to see you again soon.")
 
+
+#Mini app 9 - Snapple
+
+#Snapple is a famous tea drink brand from Queens, New York. Each bottle cap comes with a silly fun fact.
+
+import random
+fact = random.randint(0,5)
+if fact == 0:
+  print("Flamingos turn pink from eating shrimp.")
+elif fact == 1:
+  print("The only food that doesn\'t spoil is honey.")
+elif fact == 2:
+  print("Shrimp can only swim backwards.")
+elif fact == 3:
+  print("A taste bud\'s life span is about 10 days.")
+elif fact == 4:
+  print("It is impossible to sneeze while sleeping.")
+elif fact == 5:
+  print("It is illegal to sing off-key in North Carolina.")
+
